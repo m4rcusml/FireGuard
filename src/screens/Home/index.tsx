3f3037c6c33056dd20372df5612cc1aca5bbdf6f
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2024-05-15 20:41:23 -0400

    Update index.tsx

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { LinearGradient } from 'expo-linear-gradient';
+
+export function Home() {
+  return (
+    <LinearGradient colors={['#F30A0A', '#8D0606']}>
+      
+    </LinearGradient>
+  )
+}
